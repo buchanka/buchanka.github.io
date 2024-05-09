2346094b1b4a5854d5ffed9476c3b9604d6376ae
buchanka <ninagileva14@gmail.com>
2024-05-09 18:03:50 +0300

    burger menu changes

--- src/layouts/Header2.js
+++ src/layouts/Header2.js
@@ -28,22 +28,22 @@ function Header2() {
           </div>
         </Link>
 
-<div className="lg:hidden">
-    <button className="btn btn-circle swap swap-rotate" onClick={toggleMenu}>
-      <svg className={`swap-${isMenuOpen ? 'on' : 'off'} fill-current`} xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 512 512">
-            {isMenuOpen ? 
-              <polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49"/> 
-              : 
-              <path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z"/>
-            }
-        </svg>
-    </button>
-  </div>
+        <div className="lg:hidden">
+          <button className="btn btn-circle swap swap-rotate" onClick={toggleMenu}>
+            <svg className={`swap-${isMenuOpen ? 'on' : 'off'} fill-current`} xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 512 512">
+                {isMenuOpen ? 
+                <polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49"/> 
+                : 
+                <path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z"/>
+                }
+            </svg>
+          </button>
+        </div>
 
 
 
-  
-        <nav className={`lg:flex flex gap-6 items-center ${isMenuOpen ? 'block' : 'hidden'}`}>
+  {/*desktop nav */}
+        <nav className={`hidden lg:flex gap-6 items-center`}>
         {currentPath!=='/help'&&(
           <Link to="/help">
             <RiUserSettingsFill className="size-7"></RiUserSettingsFill>
@@ -75,40 +75,40 @@ function Header2() {
         </nav>
 
 
-        
-      <div className="lg:hidden fixed top-0 left-0 w-full h-screen bg-white flex flex-col justify-center items-center transition-all duration-300" style={{display: isMenuOpen ? 'flex' : 'none'}}>
-        <div className={`overlay absolute inset-0 bg-white bg-opacity-50 transition-opacity duration-300 ${isMenuOpen ? 'opacity-100' : 'opacity-0'}`} onClick={toggleMenu}></div>
-          
-           <ul className="menu p-4">
-          <Link to="/help">
-            <RiUserSettingsFill className="size-7"></RiUserSettingsFill>
-            <span className="text-darkgrey">Поддержка</span>
-          </Link>
-          {currentPath !== '/profile' && (
-            <Link to="/profile">
-              <FaUser className="size-7" />
-              <span className="text-darkgrey">Профиль</span>
+      {/*burger nav */}  
+      <div className={"lg:hidden fixed right-28 top-16 w-64 h-80 bg-white flex-col justify-center items-center transition-all duration-300 " + (isMenuOpen ? 'flex' : 'hidden')}>
+        <div className={`overlay absolute inset-0 bg-white bg-opacity-50 transition-opacity duration-300 ${isMenuOpen ? 'opacity-100' : 'opacity-0'}`} onClick={toggleMenu}>
+          <ul className="menu p-4">
+            <Link to="/help">
+              <RiUserSettingsFill className="size-7"></RiUserSettingsFill>
+              <span className="text-darkgrey">Поддержка</span>
             </Link>
-          )}
-          {currentPath !== '/community' && (
-            <Link to="/community">
-              <FaUserGroup className="size-7"></FaUserGroup>
-              <span className="text-darkgrey">Сообщество</span>
-            </Link>
-          )}
-          {currentPath !== '/messages' && (
-            <Link to="/messages">
-              <AiFillMessage className="size-7"></AiFillMessage>
-              <span className="text-darkgrey">Сообщения</span>
-            </Link>
-          )}
+            {currentPath !== '/profile' && (
+              <Link to="/profile">
+                <FaUser className="size-7" />
+                <span className="text-darkgrey">Профиль</span>
+              </Link>
+            )}
+            {currentPath !== '/community' && (
+              <Link to="/community">
+                <FaUserGroup className="size-7"></FaUserGroup>
+                <span className="text-darkgrey">Сообщество</span>
+              </Link>
+            )}
+            {currentPath !== '/messages' && (
+              <Link to="/messages">
+                <AiFillMessage className="size-7"></AiFillMessage>
+                <span className="text-darkgrey">Сообщения</span>
+              </Link>
+            )}
 
-          <Link to="/signin">
-            <MainButton text="Выйти" />
-          </Link>
-        </ul>
+            <Link to="/signin">
+              <MainButton text="Выйти" />
+            </Link>
+          </ul>
         </div>
-      </div>  
+      </div>
+      </div>
     </header>
   );
 }
